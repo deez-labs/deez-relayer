6d45081e0f2dd5590390c6584dd231cdc1e18676
Anonymous <>
2024-05-05 23:24:12 +0200

    missing import

--- relayer/src/relayer.rs
+++ relayer/src/relayer.rs
@@ -11,7 +11,7 @@ use std::{
 };
 
 use crossbeam_channel::{bounded, Receiver, RecvError, Sender};
-use dashmap::DashMap;
+use dashmap::{DashMap, DashSet};
 use histogram::Histogram;
 use jito_core::{ofac::is_tx_ofac_related, tx_cache::should_forward_tx};
 use jito_protos::{
